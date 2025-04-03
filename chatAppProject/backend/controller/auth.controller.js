8baa832991482766b7727fb040de821d0accfd10
shubh182002 <shubhamkinkar9@gmail.com>
2025-04-03 23:47:38 +0530

    Implement login and logout functionality with error handling

--- chatAppProject/backend/controller/auth.controller.js
+++ chatAppProject/backend/controller/auth.controller.js
@@ -54,6 +54,45 @@ export const signup = async (req,res,next) => {
 
 }
 
-export const login = (req,res) => {}
+export const  login =  async(req,res,next) => {
 
-export const logout = (req,res) => {}
+    try {
+        const {email,password} = req.body
+
+        const validUser =  await User.findOne({email})
+    
+        if(!validUser){
+            return next(errorHandler(404, "user not found"))
+        }
+
+        const  validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        if(!validPassword){
+            return next(errorHandler(401, "Wrong credentials"))
+        }
+
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET)
+        
+        res.cookie("access_token" , token , {httpOnly: true}).status(200).json({
+            _id : validUser._id,
+            username: validUser.username,
+            email: validUser.email,
+            profilePic : validUser.profilePic
+        }) 
+    } catch (error) {
+        next(error)     
+    }
+}
+
+export const logout = (req,res) => {
+    try {
+        res.clearCookie("access_token")
+
+        res.status(200).json({
+            message: "User has been logout successfully!"
+        })
+    } catch (error) {
+        next(error)
+        
+    }
+}
