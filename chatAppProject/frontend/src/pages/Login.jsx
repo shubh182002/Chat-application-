e7ae30107e85b3b8a052131d015931bd7fd00adf
shubh182002 <shubhamkinkar9@gmail.com>
2025-04-12 21:08:05 +0530

    refactor: restructure frontend application with routing and Tailwind CSS integration
    - Updated package.json to include Tailwind CSS and DaisyUI dependencies.
    - Refactored App component to implement React Router for navigation between Home, Login, and Signup pages.
    - Created Home, Login, and Signup components with basic structure and styling.
    - Modified main.jsx to wrap the App component with BrowserRouter for routing functionality.
    - Updated index.css to utilize Tailwind CSS and removed legacy styles.
    - Configured vite.config.js to include Tailwind CSS plugin.

---
+++ chatAppProject/frontend/src/pages/Login.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6  rounded-lg shadow-md bg-gray-700 ">
+        <h1 className="  text-3xl font-semibold text-center text-black-700">
+          Login to
+          <span className=" p-2 text-2xl font-semibold text-blue-500">Chat Application</span>
+        </h1>
+        <form action="" >
+            <div>
+                <label className="floating-label mt-5">
+                   <span>Your Email</span>
+                   <input type="text"
+                    placeholder="mail@site.com"
+                    className="input input-md"
+                  />
+                </label>
+            </div>
+            <div>
+                <label className="floating-label mt-5">
+                   <span>Password</span>
+                   <input type="password"
+                    placeholder="password"
+                    className="input input-md"
+                  />
+                </label>
+            </div>
+
+            <div>
+            <button className=" mt-5 btn btn-outline btn-success">Success</button>
+            </div>
+            
+            <Link className="block mt-2 hover:underline hover:text-blue-500 " >{"Don't"} have an account </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
