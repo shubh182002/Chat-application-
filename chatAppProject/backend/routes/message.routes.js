a8f95974929c4b0246b0270ca554a06b4f10dd2f
shubh182002 <shubhamkinkar9@gmail.com>
2025-04-12 12:03:42 +0530

    Implement getMessage functionality to retrieve messages for a conversation

--- chatAppProject/backend/routes/message.routes.js
+++ chatAppProject/backend/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { isAuthonticate } from "../middleware/verifyToken.js"
-import { sendMessage } from "../controller/message.controller.js"
+import { sendMessage, getMessage } from "../controller/message.controller.js"
 
 const router = express.Router()
 
 router.post("/send/:id",isAuthonticate ,sendMessage)
+router.get("/:id",isAuthonticate ,getMessage)
 
 export default router
